3a8ddeefda7a20b9deaf8108c326f45d108e2a14
Yashasvi Allen <yashasviallen@gmail.com>
2022-01-06 11:07:55 +0530

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -13,16 +13,17 @@ with open("README.md", "r") as f:
 
 setup(
   name='googlescrape',
-  version='0.0.2',
+  version='0.0.3',
   description='This is a python api to scrape search results from a url.',
   long_description=README,
   long_description_content_type="text/markdown",
-  url='',
+  url='https://github.com/JavaProgswing/googlescrape',
   author='JavaProgswing',
   author_email='thejavaprofessional@gmail.com',
-  license='MIT', 
+  license='MIT',
   classifiers=classifiers,
-  keywords='googlescrape', 
+  keywords='googlescrape',
   packages=find_packages(),
+  include_package_data=True,
   install_requires=['selenium >= 4.1.0','beautifulsoup4 >= 4.10.0','webdriver-manager == 3.5.2','validators >= 0.18.2']
 )
